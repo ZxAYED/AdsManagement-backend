f0b9d836aab51b0ce53af510230d8789ee12d36d
Md. Humayun kabir Sobuj <humayun506034@gmail.com>
2025-09-06 11:18:07 +0600

    create user, send verification code, verify otp logics added

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ app.use("/api", router);
 app.use(globalErrorHandler);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Server is running");
+  res.send("Server is running 🎉🎉");
 });
 
 app.use((req: Request, res: Response, next: NextFunction) => {
--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -2,16 +2,15 @@ import express from "express";
 
 import RoleValidation from "../../middlewares/RoleValidation";
 import { UserDataController } from "./user.controller";
+import { USER_ROLE } from "@prisma/client";
 const router = express.Router();
 
-router.get(
-  "/all-users",
-  UserDataController.getAllUsers
-);
+router.get("/all-users",  RoleValidation( USER_ROLE.admin),
+ UserDataController.getAllUsers);
 router.get(
   "/my-profile-info",
+  RoleValidation(USER_ROLE.customer, USER_ROLE.admin),
   UserDataController.myProfileInfo
 );
-
 
 export const UserDataRoutes = router;
