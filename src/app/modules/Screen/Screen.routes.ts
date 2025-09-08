43d231756228e4f7b994729b249a35940210a56c
Md. Humayun kabir Sobuj <humayun506034@gmail.com>
2025-09-08 11:43:18 +0600

    Schema Updated

--- src/app/modules/Screen/Screen.routes.ts
+++ src/app/modules/Screen/Screen.routes.ts
@@ -7,12 +7,11 @@ import { USER_ROLE } from "@prisma/client";
 
 const router = express.Router();
 
-router.get("/my-screens", RoleValidation(USER_ROLE.admin), ScreenController.getMySelfBanners);
 
 router.get("/", ScreenController.getAll);
 router.get("/:id", ScreenController.getById);
 router.post("/", upload.single("file"), RoleValidation(USER_ROLE.admin), ScreenController.create);
-router.patch("/:id", ScreenController.update);
-router.delete("/:id", ScreenController.remove);
+router.patch("/:id",upload.single("file"), RoleValidation(USER_ROLE.admin), ScreenController.update);
+router.delete("/:id",RoleValidation(USER_ROLE.admin) ,ScreenController.remove);
 
 export const ScreenRoutes = router;
